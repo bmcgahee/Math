089fb931df5c9fed41b7adbe7e1f5bf04d404e6d
Ben McGahee <ben.mcgahee@gmail.com>
2016-08-29 20:22:33 -0400

    Add files via upload

---
+++ WriteFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordCalculation
+{
+    class WriteFile
+    {
+        public void writeToFile(string filePath, string calculation)
+        {
+            
+            if (!File.Exists(filePath))
+            {
+                File.CreateText(filePath);
+                StreamWriter calcWriter = new StreamWriter(filePath);
+                calcWriter.Write(calculation);
+                calcWriter.Close();
+            }
+            else if (File.Exists(filePath))
+            {
+                StreamWriter calcWriter = new StreamWriter(filePath);
+                calcWriter.Write(calculation);
+                calcWriter.Close();
+            }
+
+        }
+    }
+}
