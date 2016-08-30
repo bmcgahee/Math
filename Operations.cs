34d9f5af3e8a948f573f1f6dc21fb8ce14df8cd8
Ben McGahee <ben.mcgahee@gmail.com>
2016-08-29 20:22:01 -0400

    Add files via upload

---
+++ Operations.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordCalculation
+{
+    class Operations
+    {
+        public string add(int number, int randomNumber)
+        {
+            int firstNumber = number;
+            int secondNumber = randomNumber;
+            int result = firstNumber + secondNumber;
+            string plusEquation = firstNumber + " + " + secondNumber + " = " + result;
+            return plusEquation;
+        }
+
+        public string subtract(int number, int randomNumber)
+        {
+            int firstNumber = number;
+            int secondNumber = randomNumber;
+            int result = firstNumber - secondNumber;
+            string minusEquation = firstNumber + " - " + secondNumber + " = " + result;
+            return minusEquation;
+        }
+    }
+}
